4296e6542e5f31db4dc983464e20912bb625e31d
Luboslav Metodiev <luboslav96@gmail.com>
2025-05-29 13:29:57 +0300

    modified:   js/scripts.js again

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/* global bootstrap */
 /*!
 * Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
 * Copyright 2013-2023 Start Bootstrap
@@ -11,18 +12,20 @@ window.addEventListener('DOMContentLoaded', event => {
   // Activate Bootstrap scrollspy on the main nav element
   const sideNav = document.body.querySelector('#sideNav')
   if (sideNav) {
-    new bootstrap.ScrollSpy(document.body, {
+    // Assign to variable to avoid 'no-new' warning
+    // eslint-disable-next-line no-unused-vars
+    const scrollSpy = new bootstrap.ScrollSpy(document.body, {
       target: '#sideNav',
       rootMargin: '0px 0px -40%'
     })
-  };
+  }
 
   // Collapse responsive navbar when toggler is visible
   const navbarToggler = document.body.querySelector('.navbar-toggler')
   const responsiveNavItems = [].slice.call(
     document.querySelectorAll('#navbarResponsive .nav-link')
   )
-  responsiveNavItems.map(function (responsiveNavItem) {
+  responsiveNavItems.forEach(function (responsiveNavItem) {
     responsiveNavItem.addEventListener('click', () => {
       if (window.getComputedStyle(navbarToggler).display !== 'none') {
         navbarToggler.click()
